37ef7233f9875af919c09fd4b823fabbc131fb84
boyofdoom09-maker <boyofdoom09@outlook.com>
2026-01-30 15:53:17 -0600

    Create encouragement.java

---
+++ encouragement.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Im {
+
+	public static void main(String[] args) {
+		String first = "Elijah";
+		String middle = "Steven";
+		String last = "Quigley";
+		char j = '\u2713';
+		System.out.printf("%s %s %s, you're doing great. %s %n", first, middle, last, j);
+	}
+
+}
