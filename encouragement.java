25c6eedcefa17ee344161c086a36e58b720a78f4
boyofdoom09-maker <boyofdoom09@outlook.com>
2026-01-30 16:21:15 -0600

    Update encouragement.java

--- encouragement.java
+++ encouragement.java
@@ -1,13 +1,22 @@
-package test;
 
+
+package test;
+import java.util.Scanner;
 public class Im {
 
 	public static void main(String[] args) {
-		String first = "Elijah";
-		String middle = "Steven";
-		String last = "Quigley";
+		Scanner scanner = new Scanner(System.in); 
+		System.out.println("Enter your first name");
+		String first = scanner.nextLine();
+		System.out.println("Enter your middle name");
+		String middle = scanner.nextLine();
+		System.out.println("Enter your last name");
+		String last = scanner.nextLine();
 		char j = '\u2713';
 		System.out.printf("%s %s %s, you're doing great. %s %n", first, middle, last, j);
+
+		
+		
 	}
 
 }
